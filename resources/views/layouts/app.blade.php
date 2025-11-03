fbc1eec4156920d3c6784c4cd3acba663ac0ff06
Alfin Aswar <alfin7aswar@gmail.com>
2025-11-03 16:02:09 +0700

    123123

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <meta name="description" content="POS - Bootstrap Admin Template">
-    <meta name="keywords"
-        content="admin, estimates, bootstrap, business, corporate, creative, management, minimal, modern,  html5, responsive">
-    <meta name="author" content="Dreamguys - Bootstrap Admin Template">
+    <meta name="description" content="Aplikasi Eoffice - Sistem Manajemen Perkantoran Modern">
+    <meta name="keywords" content="eoffice, perkantoran, administrasi, manajemen, modern, digital, aplikasi, responsive">
+    <meta name="author" content="Aplikasi Eoffice">
     <meta name="robots" content="noindex, nofollow">
-    <title>Lumina POS</title>
+    <title>Aplikasi Eoffice</title>
 
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('') }}assets/img/favicon.png">
@@ -171,6 +170,18 @@
                                     <span>Pembayaran</span>
                                 </a>
                             </li>
+                        </ul>
+                    </li>
+                    <li class="submenu-open">
+                        <h6 class="submenu-hdr">Laporan</h6>
+                        <ul>
+                            <li>
+                                <a href="{{ route('penawaran-harga.index') }}">
+                                    <i data-feather="file-text"></i>
+                                    <span>Omset</span>
+                                </a>
+                            </li>
+
                         </ul>
                     </li>
                     <li class="submenu-open">
